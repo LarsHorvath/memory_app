1c257dfa5c00c78914bdf169c54233bedbedfa86
Lars Horvath <lhorvath@student.ethz.ch>
2020-03-16 00:41:59 +0100

    added Game Layout for 4x3 and 5x4

--- app/src/main/java/com/memory_app/GameActivity.java
+++ app/src/main/java/com/memory_app/GameActivity.java
@@ -9,9 +9,23 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
 
         // Get Settings from previous Activity
         GameSettings gameSettings = (GameSettings) getIntent().getSerializableExtra("settings");
+
+        switch (gameSettings.getMode()){
+            case 1:
+                setContentView(R.layout.activity_game_3x2);
+                break;
+            case 2:
+                setContentView(R.layout.activity_game_4x3);
+                break;
+            case 3:
+                setContentView(R.layout.activity_game_5x4);
+                break;
+            default: setContentView(R.layout.activity_game);
+        }
+
+
     }
 }
